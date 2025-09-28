ce0e993f7865cd3bbfee27061f544f7c002ef118
infogirl <61887748+infogirl2020@users.noreply.github.com>
2025-09-28 15:46:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const listaSites = document.getElementById('sumario');
+const cardsSites = document.getElementById('projetos');
+let sites = [];
+
+fetch("sites.json")
+    .then(res => {
+        if (!res.ok) throw new Error('Erro ao carregar o arquivo JSON');
+            return res.json();
+    })
+    .then(data => {
+        sites = data;
+        carregarLista();
+        carregarSites();
+    })
+    .catch(err => {
+        listaSites.innerHTML = `<p style="color:red">Erro: ${err.message}</p>`;
+    });
+
+
+function carregarLista() {
+    listaSites.innerHTML = "";
+    sites.forEach(site => {
+        listaSites.insertAdjacentHTML("beforeend",
+            `<li><a href="${site.link}" target="_blank">${site.nome}</a></li>`
+        );
+    });
+}
+
+function carregarSites(){
+    cardsSites.innerHTML = "";
+    sites.forEach(site => {
+        cardsSites.insertAdjacentHTML("beforeend",
+            `<div class="card">
+                <a href="${site.link}" target="_blank"><img src="imagens/${site.logo}" alt="Logo do site ${site.nome}" class="logo-site"></a>
+                <h3 class="titulo-site"><a href="${site.link}" target="_blank">${site.nome}</a></h3>
+                <p class="descricao-site">${site.descricao}</p>
+            </div>`
+        );
+    });
+
+}
